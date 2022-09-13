8ce7d6a0001b869d7d9121b5b3d56184b209c09b
rafaelkloeckner <rafaelkloeckner@gmail.com>
2022-09-13 15:19:48 -0400

    Initial code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pokemon
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input = "E";                        
+            Player mainPlayer = new Player();
+            Console.WriteLine("Olá Ash! Vamos apanhar pokémons!?");
+            Console.WriteLine("Digite o caminho (N - norte, S - sul, E - este ou O - oeste):");
+            //input = Console.ReadLine();
+            mainPlayer.Move(input);
+            Console.WriteLine("Ash você foi fantástico. Apanhou " + mainPlayer.NumberPokemons() + " Pokemons.");
+        }
+    }
+}
